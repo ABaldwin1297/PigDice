12e35d999e7d0476996dd9ddfa1d82ae04a03433
ABaldwin1297 <abaldwin1297@gmail.com>
2022-05-06 15:38:24 -0400

    Add project files.

---
+++ PigDice/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using PigDice;
+
+const int highScore = 180;
+long games = 0;
+
+var pigdice = new PigDiceGame();
+var score = pigdice.Play();
+    while(score < highScore) {
+    games++;
+    if(games % 100 == 0) {
+        Console.WriteLine($"Number of games is {games}");
+    }
+    score = pigdice.Play();
+}
+Console.WriteLine(score);
